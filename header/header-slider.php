68349719559dc281cc83465cc02c4fb1e34bb412
stilesyu <yuxiaochen886@gmail.com>
2021-08-08 21:54:07 +0800

    page-slider编写完成

--- header/header-slider.php
+++ header/header-slider.php
@@ -11,29 +11,26 @@
             <ul class="slider-area-card-swiper-wrapper">
                 <!--slider card one-->
                 <li class="slider-area-card-swiper-wrapper-item">
-                    <a href="<?php echo cardroom_options("general_slider_url") ?>">
+                    <a href="<?php echo cardroom_options("general_slider_url_1") ?>">
                         <img src="<?php echo wp_get_attachment_image_src(cardroom_options("general_image_1"))[0] ?>">
                     </a>
                 </li>
                 <!--slider card two-->
                 <li class="slider-area-card-swiper-wrapper-item">
-                    <a>
-                        <img src="https://cdn.sspai.com/article/6c4ba71d-d13c-dc41-bd6a-8633c7f6defc.png?imageMogr2/auto-orient/quality/95/thumbnail/!528x288r/gravity/Center/crop/528x288/interlace/1"
-                             alt="Avatar">
+                    <a href="<?php echo cardroom_options("general_slider_url_2") ?>">
+                        <img src="<?php echo wp_get_attachment_image_src(cardroom_options("general_image_2"))[0] ?>">
                     </a>
                 </li>
                 <!--slider card three-->
                 <li class="slider-area-card-swiper-wrapper-item">
-                    <a>
-                        <img src="https://cdn.sspai.com/article/2a4ca250-8910-8f32-724c-02aad1eb9914.jpeg?imageMogr2/auto-orient/quality/95/thumbnail/!528x288r/gravity/Center/crop/528x288/interlace/1"
-                             alt="Avatar">
+                    <a href="<?php echo cardroom_options("general_slider_url_3") ?>">
+                        <img src="<?php echo wp_get_attachment_image_src(cardroom_options("general_image_3"))[0] ?>">
                     </a>
                 </li>
                 <!--slider card four-->
                 <li class="slider-area-card-swiper-wrapper-item">
-                    <a>
-                        <img src="https://cdn.sspai.com/article/648ccd64-5e01-e785-3d1c-e31bc7b6a57e.jpg?imageMogr2/auto-orient/quality/95/thumbnail/!528x288r/gravity/Center/crop/528x288/interlace/1"
-                             alt="Avatar">
+                    <a href="<?php echo cardroom_options("general_slider_url_4") ?>">
+                        <img src="<?php echo wp_get_attachment_image_src(cardroom_options("general_image_4"))[0] ?>">
                     </a>
                 </li>
             </ul>
