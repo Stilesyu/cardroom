a773bcf766d24f2a03d0ee544ae3e12adaa67f75
stilesyu <yuxiaochen886@gmail.com>
2021-08-15 00:04:50 +0800

    文章内容重构完成

--- single.php
+++ single.php
@@ -6,24 +6,26 @@
  */
 
 ?>
-<?php get_header();
-
-?>
-<?php
-if (cardroom_options("general_show_up_in_the_post")){
-    get_template_part('/header/header', 'slider');
-}
-get_template_part('/header/header', 'category');?>
-<section class="single-area">
-    <section class="single-area-contain">
-        <?php
-        while (have_posts()):
-            the_post();
-            get_template_part('templates/content/content', 'single');
-        endwhile;
-        ?>
-    </section>
-</section>
+<?php get_header(); ?>
+<div class="single-container">
+        <section class="single-container-section">
+            <?php
+            //advertise bar
+            if (cardroom_options("general_show_up_in_the_post")) {
+                get_template_part('/header/header', 'slider');
+            }
+            //navigation bar
+            get_template_part('/header/header', 'category');
+            while (have_posts()):
+                the_post();
+                get_template_part('templates/content/content', 'single');
+            endwhile;
+            ?>
+        </section>
+        <div class="single-container-sidebar">
+            <?php get_sidebar() ?>
+        </div>
+</div>
 <?php
 get_footer()
 ?>
--- templates/content/content-single.php
+++ templates/content/content-single.php
@@ -8,30 +8,23 @@
 ?>
 <?php
 $classes = array(
-    'single-area-template'
+    'content-single-container'
 );
 ?>
 <!--post content-->
 <article id="post-<?php the_ID(); ?>"<?php post_class($classes) ?>>
-
-    <div class="single-area-post">
-        <div class="single-area-post-sidebar-container-left">
-            <?php get_sidebar('left') ?>
-        </div>
-        <div class="single-area-post-content">
-            <header class="single-area-post-content-header">
-                <?php if (get_the_title()): ?>
-                    <h1 class="single-area-post-content-header-title"><?php the_title() ?></h1>
-                <?php endif ?>
-            </header>
-            <div class="single-area-post-content-content">
-                <?php
-                the_content();
-                ?>
-            </div>
-        </div>
-        <div class="single-area-post-sidebar-container-right">
-            <?php get_sidebar('right') ?>
+    <div class="content-single-container-article">
+        <!--article header-->
+        <header class="content-single-container-article-content-header">
+            <?php if (get_the_title()): ?>
+                <h1 class="content-single-container-article-content-header-title"><?php the_title() ?></h1>
+            <?php endif ?>
+        </header>
+        <!--article content-->
+        <div class="content-single-container-article-content">
+            <?php
+            the_content();
+            ?>
         </div>
     </div>
 </article>
