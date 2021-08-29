ce9d9d2931e3c39d8de58f6ae3aa144080c8c452
stilesyu <yuxiaochen886@gmail.com>
2021-08-29 23:44:13 +0800

    demo:碰到自动隐藏

--- sidebar.php
+++ sidebar.php
@@ -1,13 +1,25 @@
-
-
 <?php
 if (!empty(cardroom_options('sidebar_show_up_donate_image'))) {
     get_template_part('templates/sidebars/sidebar', 'official-accounts');
 }
-if (is_single()){
+if (is_single()) {
     get_template_part('templates/sidebars/sidebar', 'catalogue');
 }
 ?>
-<div style="background: #0c88b4;width: 500px;height: 500px ;margin-top: 30px" >
+<div id="temp" style="background: #0c88b4;width: 500px;height: 500px ;margin-top: 30px" class="animate__animated">
 
 </div>
+
+<script>
+    window.addEventListener("scroll", function () {
+        const catalogue = document.getElementsByClassName('sidebar-catalogue');
+        const temp = document.getElementById('temp');
+        const distance = catalogue.item(0).getBoundingClientRect().y - temp.getBoundingClientRect().y;
+        console.log(distance);
+        if (distance > -400) {
+            temp.classList.add('animate__fadeOutRight')
+        }
+    })
+
+
+</script>
