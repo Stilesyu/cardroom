05727a76e888514b64844c4ae9a3a8af9409eefe
stilesyu <yuxiaochen886@gmail.com>
2021-09-08 23:45:04 +0800

    引入jq

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="profile" href="https://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
+    <?php
+    wp_enqueue_script("jquery");
+    wp_head(); ?>
 </head>
 <!--加载page-header模板-->
 <body  <?php body_class(); ?>>
